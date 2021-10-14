8f2676500b49260781662160f87b899c4e054feb
fozil-lab <davlatyorovfozil2003@gmail.com>
2021-10-14 12:53:36 +0500

    MAZGI UJE

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,11 +3,13 @@ const callsRouter = require('./calls')
 const productsRouter = require('./products')
 const usersRouter = require('./users')
 const ordersRouter = require('./orders')
+const categoriesRouter = require('./categories')
 
 module.exports = [
     carouselRouter,
     callsRouter,
     productsRouter,
     usersRouter,
-    ordersRouter
+    ordersRouter,
+    categoriesRouter
 ]
